09cae179a2a059ecf9456004be0b8354fbb8ecdf
Sunil Pore <sunilyahoo95@gmail.com>
2020-10-17 20:10:24 +0530

    fix glide image url loading issue

--- app/src/main/java/com/androidtesteg/ui/movie/MovieDetailFragment.kt
+++ app/src/main/java/com/androidtesteg/ui/movie/MovieDetailFragment.kt
@@ -1,6 +1,8 @@
 package com.androidtesteg.ui.movie
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +11,10 @@ import com.androidtesteg.R
 import com.androidtesteg.data.Movie
 import com.androidtesteg.data.source.MoviesRemoteDataSource
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import kotlinx.android.synthetic.main.fragment_movie_detail.*
 
 class MovieDetailFragment : Fragment(){
