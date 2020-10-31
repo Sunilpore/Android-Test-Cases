07cacfc174a483085ffacb4dcb67da8eea49ed8f
Sunil Pore <sunilyahoo95@gmail.com>
2020-10-31 21:24:22 +0530

    add fragment navigation from activity

--- app/src/androidTest/java/com/androidtesteg/ui/movie/MovieFragmentTestSuite.kt
+++ app/src/androidTest/java/com/androidtesteg/ui/movie/MovieFragmentTestSuite.kt
@@ -11,6 +11,7 @@ import org.junit.runners.Suite
 @Suite.SuiteClasses(
     MovieDetailFragmentTest::class,
     DirectorsFragmentTest::class,
-    StarActorsFragmentTest::class
+    StarActorsFragmentTest::class,
+    MovieNavigationTest::class
 )
 class MovieFragmentTestSuite
